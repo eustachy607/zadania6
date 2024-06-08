8a4a5e9979cf8e48f4c0987fbc4c3d48b249f1e2
eustachy607 <164636124+eustachy607@users.noreply.github.com>
2024-06-08 22:22:56 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
-const { faker } = require('@faker-js/faker');
 const app = express();
 
-const User = require('./models/user'); 
-const Book = require('./models-book'); 
-// Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 mongoose.connect('mongodb://localhost:27017/authdb', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', UserSchema);
 
 const hashPassword = (password) => {
     const salt = crypto.randomBytes(16).toString('hex');
@@ -22,12 +24,10 @@ const hashPassword = (password) => {
     return { salt, hash };
 };
 
-
 const validatePassword = (password, salt, hash) => {
     const hashVerify = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
     return hash === hashVerify;
 };
-
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
@@ -41,7 +41,6 @@ app.post('/register', async (req, res) => {
         res.status(500).send('Error registering user');
     }
 });
-
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
@@ -66,47 +65,9 @@ app.post('/login', async (req, res) => {
     }
 });
 
-
-app.get('/books/count', async (req, res) => {
-    try {
-        const count = await Book.countDocuments();
-        res.status(200).json({ count });
-    } catch (err) {
-        res.status(500).send('Error fetching count');
-    }
-});
-
-
-app.get('/books', async (req, res) => {
-    const { genre, minPages, minYear } = req.query;
-    let query = {};
-
-    if (genre) {
-        query.genre = genre;
-    }
-
-    if (minPages) {
-        query.pages = { $gte: Number(minPages) };
-    }
-
-    if (minYear) {
-        query.publishYear = { $gte: Number(minYear) };
-    }
-
-    try {
-        const books = await Book.find(query);
-        res.status(200).json(books);
-    } catch (err) {
-        res.status(500).send('Error fetching books');
-    }
-});
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
